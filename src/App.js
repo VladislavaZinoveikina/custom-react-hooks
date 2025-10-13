7eb0d798a9923fdde90c88c7760f32b70e0e042b
Владислава Зиновейкина <107256699+VladislavaZinoveikina@users.noreply.github.com>
2025-10-13 12:28:53 +0200

    useDebounce

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
-import List from "./components/List";
+import React, { useState } from "react";
+import useDebounce from "./hooks/useDebounce";
 
 function App() {
+  const [value, setValue] = useState('');
+  const debouncedSearch = useDebounce(search, 500);
+
+  function search(query) {
+    fetch(`https://jsonplaceholder.typicode.com/todos?query=` + query)
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+      })
+  };
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+    debouncedSearch(e.target.value);
+  };
+
 
   return (
     <div>
-      <List />
+      <input type="text" value={value} onChange={onChange}/>
     </div>
   );
 }
